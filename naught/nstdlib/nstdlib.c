ac9c050991daf88b5006d2c23f5f001a718a8a72
codyohl <codyohl@cs.washington.edu>
2015-12-10 22:59:02 -0800

    BLOO & COCO GET THE STANGS

--- naught/nstdlib/nstdlib.c
+++ naught/nstdlib/nstdlib.c
@@ -35,6 +35,6 @@ char * nstr_plus(char * s1, char * s2) {
 	nstr->str = (char *) malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
 	nstr->len = nstrlen(s1) + nstrlen(s2);
 	strcpy(nstr->str, s1);
-	strcpy(nstr->str + nstrlen(s1), s2);
+	strcat(nstr->str, s2);
 	return nstr->str;
 }
