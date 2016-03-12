e6f16bf94ec8ab92e91ce300fce1f0b6d1a2648a
Kevin <kc55@uw.edu>
2016-03-12 10:40:51 -0800

    h

--- naught/naughtfiles/example.h
+++
@@ -1,9 +0,0 @@
-#ifndef _EXAMPLE_H
-#define _EXAMPLE_H
-
- function fact(int x, pointer p); # 'function' means 'returns an int'
-sfunction initLabel();            # 'sfunction' means 'returns a string'
-
-int globalInt;
-
-#endif //_EXAMPLE_H
--- naught/nstdlib/createNaughtLibrary.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc -c -Wall -g -c nstdlib.c -o nstdlib.o
-ar -rcs libnaught.a nstdlib.o
